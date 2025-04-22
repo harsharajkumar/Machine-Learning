f6f98c37a7d27894c0282f6ea59469cb226815aa
HARSHA R <150285881+harsharajkumar@users.noreply.github.com>
2025-04-22 12:24:33 +0530

    Add files via upload

---
+++ Linear Regression from Scratch/Linear Regression From Scratch.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, lr=0.001, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weight = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weight = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            y_pred = np.dot(X, self.weight) + self.bias
+            dw = (1 / n_samples) * np.dot(X.T, (y_pred - y))
+            db = (1 / n_samples) * np.sum(y_pred - y)
+
+            self.weight -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        return np.dot(X, self.weight) + self.bias
